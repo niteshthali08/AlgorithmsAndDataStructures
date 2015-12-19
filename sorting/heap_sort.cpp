0a391b423b1a87f367a0949eab7128fbe3da7ce2
Nitesh Thali <niteshthali08@gmail.com>
2015-12-18 23:07:50 -0700

    Heap sort and merge sort in C++.

---
+++ sorting/heap_sort.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+/* max heap code to sort array in decreasing order, in place sorting.
+
+Time complexity for buildiing a max heap: O(n)
+Time complexity to sort: O(n*lgn)
+*/
+int left (int i)
+{
+	return 2*i + 1;	
+}
+int right (int i)
+{
+	return 2*i + 2;
+}
+void swap(int *first, int *last)
+{
+	int t = *first;
+	*first = *last;
+	*last = t;
+	//cout << "inside swap function" <<*first <<" "<<*last;
+}
+void max_heapify(int a[], int index, int size)
+{
+	int l = left(index);
+	int r = right(index);
+	int largest = index;
+	if( l <= size and a[l] > a[largest])
+		largest = l;
+	if (r <=size  and a[r] > a[largest])
+		largest = r;
+	if (largest != index)
+	{
+		swap(&a[index], &a[largest]);
+		max_heapify(a, largest, size);
+	}
+ }
+void build_max_heap(int a[], int length)
+{ 	// start calling max heapify function from size/2; heapify will take care of leaf nodes.
+	
+	for (int i = length/2 - 1; i >=0; --i)
+	{   //cout << "building max heap" <<i <<endl;
+		max_heapify(a, i, length); 
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {50, 60, 10, 100, 20, 90, 70};
+	//int a[] = {10};
+	//int a[] = {10,20,30,40,50,60,70};
+	//int a [] = {70,60,50,40,30,20,10};
+	int length = sizeof(a)/sizeof(int);
+	//cout << length << endl;
+	build_max_heap(a, length-1);
+	int size = length -1;
+	for (int i = length - 1; i > 0; --i) 
+	{		
+			
+			swap(&a[0], &a[i]);
+			max_heapify(a, 0, --size);
+		
+	}
+	cout <<"sorted: ";
+	for (int i = 0; i < length; ++i)
+	{
+		cout << a[i] << " ";void swap(int *first, int *last);
+	}
+	cout << endl;
+	return 0;
+}
---
+++ sorting/merge_sort.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+// merge sort uses divide and conquer strtegy to sort the elements.
+//Running time: O(n), Space complexity: O(n)
+void merge(int a[], int low, int mid, int high)
+{
+	//cout << "low:mid:high " << low << ":"<< mid <<":"<< high<<endl;
+	int l1 = mid - low +1;
+	int l2 = high - mid;
+	int b1[l1], b2[l2];
+	for (int i = 0; i <l1; ++i)
+	{
+		b1[i] = a[low + i];
+	}
+	for (int j = 1; j <=l2; ++j)
+	{
+		b2[j-1] = a[mid + j];
+	}
+	int i = 0, j = 0;
+	int k = low;
+	while (i < l1 && j<l2)
+	{
+		if(b1[i] > b2[j])
+		{
+			a[k++] = b2[j++];
+
+		}
+		else
+		{
+			a[k++] = b1[i++];
+		}
+	}
+	if (i == l1)
+		{
+			
+			for (int l = j; l<l2; l++)
+				a[k++] = b2[l];
+		}
+	else
+	{
+		for (int l = i; l<l1; l++)
+				a[k++] = b1[l];
+	}	
+	// cout<< "array: ";
+	// for (int i = low; i <= high; ++i)
+	// {
+	// 	cout << a[i]<< " ";
+	// }
+	// cout << endl;
+}	
+
+
+void merge_sort(int a[], int low, int high)
+{
+	if (low < high)
+	{
+		int mid = (low + high)/2;
+		merge_sort(a,low, mid);
+		merge_sort(a, mid+1, high);
+		merge(a,low, mid, high);
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int a[] = {30, 10, 70, 60, 50, 90, 0, 20};
+	//int a[] = {10,20,30,40,50,55,60, 5};
+	//int a[] = {70,60,50,40,30,20,10};
+	//cout << "length: " << sizeof(a)/sizeof(int)-1 <<endl;
+	merge_sort(a, 0,sizeof(a)/sizeof(int)-1);// //note -1 here... important to make index calculation easy
+	
+	cout << "sorted: ";
+	for (int i = 0; i < sizeof(a)/sizeof(int); ++i) 
+	 	cout << a[i] <<" ";
+	 cout <<endl;
+	return 0;
+}
