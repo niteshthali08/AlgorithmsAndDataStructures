b3a4428022bcd4d7ea416c25fa7aae9856d031a1
Nitesh Thali <niteshthali08@gmail.com>
2016-07-22 23:12:09 -0700

    adding programs on Binary trees

---
+++ Mathematical Computation/Mathematical Computation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mathematical Computation
+//
+//  Created by Nitesh Thali on 7/13/16.
+//  Copyright © 2016 Nitesh Thali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
